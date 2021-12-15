1af86faca3577e2984b0c6a95a182b6de33c1e8f
jacobjolibois <jacobjolibois@hey.com>
2021-12-15 12:55:40 -0600

    updated README, created Form.js component and imported it to App.js

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+      </header>
+      <div className='container'>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
